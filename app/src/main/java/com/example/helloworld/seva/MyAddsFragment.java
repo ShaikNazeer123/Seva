e1ce007b7adfb1006077a11667c3950ce01c1a74
tusharpahuja <tushar.pahuja2015@vit.ac.in>
2018-03-18 11:46:57 +0530

    pta nhi

--- app/src/main/java/com/example/helloworld/seva/MyAddsFragment.java
+++ app/src/main/java/com/example/helloworld/seva/MyAddsFragment.java
@@ -65,10 +65,10 @@ public class MyAddsFragment extends Fragment {
     private DatabaseReference mDatabaseMisc;
     private DatabaseReference mDatabaseUsersMisc;
 
-    Map<String,Map<String,String> > foodMap;
-    Map<String,Map<String,String> > clothesMap;
-    Map<String,Map<String,String> > booksMap;
-    Map<String,Map<String,String> > miscMap;
+    Map<String,Map<String,Object> > foodMap;
+    Map<String,Map<String,Object> > clothesMap;
+    Map<String,Map<String,Object> > booksMap;
+    Map<String,Map<String,Object> > miscMap;
 
     private String name;
     private String phonenumber;
@@ -79,6 +79,9 @@ public class MyAddsFragment extends Fragment {
     private String image;
 
     private String uId;
+
+    private String postUID;
+    private String imageUri;
 
     public MyAddsFragment() {
         // Required empty public constructor
